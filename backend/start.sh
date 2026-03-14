64e6f18cca325ab3e846ed4e3a882dd0d7ebc327
arijitswain12 <arijit.swain@mindtide.com>
2026-03-14 21:30:45 +0530

    fix railway port

--- backend/start.sh
+++ backend/start.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn hrms_project.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --threads 4 --timeout 60
+gunicorn hrms_project.wsgi:application --bind 0.0.0.0:8080 --workers 2 --threads 4 --timeout 60
