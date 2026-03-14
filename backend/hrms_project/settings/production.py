f50f6e6fd10bee5366be19c8d8143729e5e18f99
arijitswain12 <arijit.swain@mindtide.com>
2026-03-15 00:55:37 +0530

    Backend and Frontend Deployment

--- backend/hrms_project/settings/production.py
+++ backend/hrms_project/settings/production.py
@@ -10,8 +10,8 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"] # noqa: F405
 
 # CORS: only allow the deployed frontend
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split()  # noqa: F405
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["https://hrms-lite-olo9.onrender.com/api"]  # noqa: F405
 
 # Security headers
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
