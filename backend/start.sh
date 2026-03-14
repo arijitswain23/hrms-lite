3f5dd8dab1606e287f11fd42ee41c6d0e922fb42
arijitswain12 <arijit.swain@mindtide.com>
2026-03-14 23:59:16 +0530

    Render Deployment

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-
-gunicorn hrms_project.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --threads 4 --timeout 60
+gunicorn hrms_project.wsgi:application --bind 0.0.0.0:$PORT
